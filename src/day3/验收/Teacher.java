bd4b7af698594065257ec39e2b502f96408c7a55
1234abcd693 <822690554@qq.com>
2026-08-02 20:24:06 +0800

    更新 Teacher.java

--- src/day3/验收/Teacher.java
+++ src/day3/验收/Teacher.java
@@ -1,4 +1,25 @@
 package day3.验收;
 
-public class Teacher {
+public class Teacher extends Person{
+    private String subject;
+    public Teacher(){
+        this(" ",0," ");
+    }
+    public Teacher(String name,int age,String subject){
+        super(name,age);
+        this.subject=subject;
+    }
+    @Override
+    public void showInfo(){
+        System.out.println("教师信息：");
+        super.showInfo();
+        System.out.println("科目："+subject);
+    }
+    
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject=subject;
+    }
 }
