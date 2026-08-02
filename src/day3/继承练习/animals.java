ab28a2a30be2d69ca0df9dc0b05640cc6df9a73b
1234abcd693 <822690554@qq.com>
2026-08-02 19:20:20 +0800

    定义父类animals

--- src/day3/继承练习/animals.java
+++ src/day3/继承练习/animals.java
@@ -2,5 +2,7 @@ package day3.继承练习;
 
 public class animals {
     private String name;
-    public abstract void eat();
+    public abstract void eat(){
+        System.out.println("动物在吃东西");
+    }
 }
