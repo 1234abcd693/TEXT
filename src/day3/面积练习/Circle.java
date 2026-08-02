8f02319746337e75f9021d9be7ff10fe4e3280e1
1234abcd693 <822690554@qq.com>
2026-08-02 19:48:35 +0800

    更新 Circle.java
    写清楚了抽象方法只写方法 具体的类记录传递数据

--- src/day3/面积练习/Circle.java
+++ src/day3/面积练习/Circle.java
@@ -1,4 +1,16 @@
 package day3.面积练习;
 
-public class Circle {
+public class Circle extends Shape{
+    public final double PI=3.14;
+    private double r;
+    public double getR(){
+        return r;
+    }
+    public void setR(double r){
+        this.r=r;
+    }
+    @Override
+    public double getArea(){
+        return PI*r*r;
+    }
 }
