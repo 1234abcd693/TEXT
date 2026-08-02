3c18fcc33c3a18eb8f2ed6f290311e673d05e0b5
1234abcd693 <822690554@qq.com>
2026-08-02 20:11:56 +0800

    更新 Person.java
    完成父类Person

--- src/day3/验收/Person.java
+++ src/day3/验收/Person.java
@@ -1,4 +1,39 @@
 package day3.验收;
 
-public class Person {
+public abstract class Person {
+    private String name;
+    private int age;
+
+
+    public Person(){
+    }
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public abstract void showInfo();
+
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        if(0<=age&&age<=100){
+            this.age=age;
+        }
+        else{
+            System.out.println("年龄非法");
+        }
+
+
+    }
+    
 }
