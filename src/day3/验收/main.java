f7654bdbe4dacc906f173aef15a9e2dfb88bfadc
1234abcd693 <822690554@qq.com>
2026-08-02 20:32:39 +0800

    更新 main.java
    Person and Teacher,Student  增加多态思想

--- src/day3/验收/main.java
+++ src/day3/验收/main.java
@@ -2,8 +2,8 @@ package day3.验收;
 
 public class main {
     public static void main(String[] args) {
-        Student s1=new Student("张三",18,"计算机");
-        Teacher t1=new Teacher("李四",30,"数学");
+        Person s1=new Student("张三",18,"计算机");
+        Person t1=new Teacher("李四",30,"数学");
         s1.showInfo();
         t1.showInfo();
 
