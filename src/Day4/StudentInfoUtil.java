57f3544811993ffda5c97bf370e36b6629ebdb83
1234abcd693 <822690554@qq.com>
2026-08-03 21:22:33 +0800

    更新 StudentInfoUtil.java
    用字符串的方法来解析学生信息，用Integer和int来改变数据类型 不太熟悉需要多加强

--- src/Day4/StudentInfoUtil.java
+++ src/Day4/StudentInfoUtil.java
@@ -1,4 +1,10 @@
 package Day4;
 
 public class StudentInfoUtil {
+    public static Student parse(String info){
+        String[] arr=info.split(",");
+        int id=Integer.parseInt(arr[0]);
+        int age=Integer.parseInt(arr[2]);
+        return new Student(id,arr[1],age,arr[3]);
+    }
 }
