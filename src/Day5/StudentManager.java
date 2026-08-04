ccce17b854917a884999db9b446dd60ed1a63e4c
1234abcd693 <822690554@qq.com>
2026-08-05 00:48:28 +0800

    更新 StudentManager.java
    完善studentmanager方法 正在写方法

--- src/Day5/StudentManager.java
+++ src/Day5/StudentManager.java
@@ -1,4 +1,34 @@
 package Day5;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class StudentManager {
+    private ArrayList<Student> students=new ArrayList<Student>();
+    public void addStudent(Student student){
+        students.add(student);
+    }
+    public void showStudents(){
+        for(Student student:students){
+            System.out.println(student.getInfo());
+        }
+    }
+    public void Byid(int id){
+        for(Student student:students){
+            if(student.getId()==id){
+                System.out.println(student.getInfo());
+            }
+        }
+    }
+
+    public void deleteStudent(int id){
+        for(Student student:students){
+            if(student.getId()==id){
+                students.remove(student);
+                System.out.println("删除成功");
+            }
+        }
+    }
+
+    public double getMaxScoreStudent()
 }
