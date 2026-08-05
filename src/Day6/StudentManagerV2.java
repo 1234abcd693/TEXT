34335d40a28dffdfb10a49dcd17be1c8da50a2bb
1234abcd693 <822690554@qq.com>
2026-08-05 15:18:24 +0800

    更新 StudentManagerV2.java
    学习用哈希表管理学生系统

--- src/Day6/StudentManagerV2.java
+++ src/Day6/StudentManagerV2.java
@@ -1,4 +1,29 @@
 package Day6;
 
+import java.util.HashMap;
+
 public class StudentManagerV2 {
+    private HashMap<Integer,Student> map = new HashMap<>();
+    public void addStudent(Student student){
+        map.put(student.getId(),student);
+    }
+    public Student findById(int id){
+        return map.get(id);
+    }
+
+    public void deleteStudent(int id){
+        map.remove(id);
+    }
+
+
+    public boolean existsStudent(int id){
+        return map.containsKey(id);
+    }
+
+    public void showStudents(){
+        for(Integer id:map.keySet()){
+            Student student = map.get(id);
+            System.out.println(student.getInfo());
+        }
+    }
 }
